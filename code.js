9951ba5fd28b02c635218fccfe3f87bc07e92901
XavierC-13 <113617551+XavierC-13@users.noreply.github.com>
2022-10-20 08:26:40 -0400

    correction html plus addition de javascript

--- code.js
+++ code.js
@@ -11,3 +11,18 @@ function goToChapter(){
     console.log(chaptersObj["beginningObj"].subtitle);
     console.log(chaptersObj["beginningObj"].text);
 }
+
+
+let chaptersObj = {
+    mise_en_contexte : {
+      subtitle: "Qu'elle heure est-il?",
+      text: "Tu es à une fête, tu en as profité le plus possible, tu regarde ton téléphone et réalise qu'il est devenue tard.  Tu es vraiment saoul et tu dois rentrer chez toi. Comment vas-tu t'y prendre?",
+      img: "assets/img/cell_phone_time.jpg",
+      options: [
+        {
+          text: "",
+          action: "goToChapter(``)"
+        }
+      ]
+    },
+}
